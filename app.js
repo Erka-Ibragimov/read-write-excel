c2e967409e3f3c2bc28e2b64172030132e2e2c65
Erka-Ibragimov <erkaibragimov@gmail.com>
2023-03-07 16:42:21 +0500

    write excel

--- app.js
+++ app.js
@@ -2,11 +2,10 @@ const path = require("path");
 const fs = require("fs");
 const json2xls = require("json2xls");
 const readFileExcel = async () => {
-  const pathToFile = path.resolve(__dirname, "./files/01-28-02-2023-mts.json");
-
+  const pathToFile = path.resolve(__dirname, "./files/text.txt");
   const data = fs.readFileSync(pathToFile, "utf8", (err, data) => {
     if (err) {
-      console.error(err);
+      console.error(`ReadFile ${err}`);
       return;
     }
     return data;
@@ -14,20 +13,27 @@ const readFileExcel = async () => {
 
   const dataJSON = JSON.parse(data);
 
-  //   dataJSON.data.forEach((element) => {
-  //     element.id = element.id.toString();
-  //     element.amount = (element.amount / 100).toString();
-  //     element.nominal = (element.nominal / 100).toString();
-  //     element.rate = element.rate.toString();
-  //     element.currency = element.currency.toString();
-  //   });
+  dataJSON.data.forEach((element) => {
+    element.id = element.id.toString();
+    element.amount = (element.amount / 100).toString();
+    element.nominal = (element.nominal / 100).toString();
+    element.rate = element.rate.toString();
+    element.currency = element.currency.toString();
+  });
 
   const xls = await json2xls(dataJSON.data, {
     output: "base64",
   });
 
+  fs.mkdir(path.join(__dirname, "newFiles"), { recursive: true }, (err) => {
+    if (err) {
+      console.error(`MKDIR ${err}`);
+      return;
+    }
+  });
+
   fs.writeFileSync(
-    path.resolve(__dirname, "./newFiles/01-28-mts.xlsx"),
+    path.resolve(__dirname, "./newFiles/text.xlsx"),
     xls,
     "binary"
   );
